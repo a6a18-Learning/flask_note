268e9374015b7283a9c99ada63e18923693ccf91
Tom Shen <tom.shen@tgc-taiwan.com.tw>
2020-04-01 15:14:56 +0800

    新增 測試/練習 app 和更新內容

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # flask_note
 My Flask Note
 
+參考資料：[http://docs.jinkan.org/docs/flask/](http://docs.jinkan.org/docs/flask/)
 ### 調試模式
 
 雖然`run()`方法適用於啟動本地的開發服務器，但是你每次修改代碼後都要手動重啟它。這樣並不夠優雅，而且Flask可以做到更好。如果你啟用了調試支持，服務器會在代碼修改後自動重新載入，並在發生錯誤時提供一個相當有用的調試器。
---
+++ app/app1.py
@@ -0,0 +1,26 @@
+from flask import Flask, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Flask Hi'
+    
+@app.route('/login') 
+def login():
+    pass
+ 
+@app.route('/user/<username>') 
+def profile(username):
+    pass
+    
+
+with app.test_request_context(): 
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login' , next='/'))
+    print(url_for('profile', username='John Doe'))
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
